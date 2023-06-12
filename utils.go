846d0c31258dda84a2ed69a96db99e95cfcc7fa0
Noooste <noste@MacBook-Air-de-Noste.local>
2023-06-12 18:59:25 +0800

    [+] GetLastChromeVersion for chrome 111

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	tls "github.com/Noooste/utls"
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/text/runes"
@@ -155,4 +156,91 @@ func CleanAddress(address string) string {
 	s, _, _ := transform.String(t, address)
 	var punctuationReplace = regexp.MustCompile(`[[:punct:]]`)
 	return punctuationReplace.ReplaceAllString(s, ` `)
+}
+
+// GetLastChromeVersion apply the latest Chrome version
+// Current Chrome version : 111
+func GetLastChromeVersion() *tls.ClientHelloSpec {
+	extensions := []tls.TLSExtension{
+		&tls.UtlsGREASEExtension{},
+		&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+			{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+			{Group: tls.X25519},
+		}},
+		&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+		&tls.SNIExtension{},
+		&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+			tls.ECDSAWithP256AndSHA256,
+			tls.PSSWithSHA256,
+			tls.PKCS1WithSHA256,
+			tls.ECDSAWithP384AndSHA384,
+			tls.PSSWithSHA384,
+			tls.PKCS1WithSHA384,
+			tls.PSSWithSHA512,
+			tls.PKCS1WithSHA512,
+		}},
+		&tls.UtlsExtendedMasterSecretExtension{},
+		&tls.SessionTicketExtension{},
+		&tls.SCTExtension{},
+		&tls.RenegotiationInfoExtension{},
+		&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+			tls.PskModeDHE,
+		}},
+		&tls.ApplicationSettingsExtension{SupportedALPNList: []string{"h2", "http/1.1"}},
+		&tls.CompressCertificateExtension{Algorithms: []tls.CertCompressionAlgo{
+			tls.CertCompressionBrotli,
+		}},
+		&tls.SupportedVersionsExtension{Versions: []uint16{
+			tls.GREASE_PLACEHOLDER,
+			tls.VersionTLS13,
+			tls.VersionTLS12,
+		}},
+		&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+			tls.GREASE_PLACEHOLDER,
+			tls.X25519,
+			tls.CurveP256,
+			tls.CurveP384,
+		}},
+		&tls.StatusRequestExtension{},
+		&tls.SupportedPointsExtension{SupportedPoints: []byte{
+			0x00, // pointFormatUncompressed
+		}},
+		&tls.UtlsGREASEExtension{},
+		&tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
+	}
+
+	extensionsLength := len(extensions)
+	lastTwo := extensionsLength - 2
+
+	rand.Shuffle(extensionsLength, func(i, j int) {
+		if i >= lastTwo || j >= lastTwo || i == 0 || j == 0 {
+			return
+		}
+		extensions[i], extensions[j] = extensions[j], extensions[i]
+	})
+
+	return &tls.ClientHelloSpec{
+		CipherSuites: []uint16{
+			tls.GREASE_PLACEHOLDER,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+		CompressionMethods: []byte{
+			0x00, // compressionNone
+		},
+		Extensions: extensions,
+	}
 }
